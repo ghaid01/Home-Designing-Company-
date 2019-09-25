f049b32d7912c1f66244dd0ed8e37fecbd9e03be
ghaid <ghaidzahran2001@gmail.com>
2019-09-25 19:55:02 +0300

    gzg

--- c01/add.js
+++ c01/add.js
@@ -7,6 +7,8 @@ var room = prompt('what size is your room?! 5-10');
 var someone;
 
 // processing:
+
+
 if (room > 5) {
   someone = 'Mr Micheal will design your room!';
 } else if (room < 5 ) {
@@ -22,22 +24,37 @@ return someone;
 
 var showRoom = function() {
 // input
+var total;
 var room = prompt('what room would you like to design?'); 
 var picture;
 
 //processing
-if (room == 'living room'){
-  picture = '<img src="https://freshome.com/wp-content/uploads/2018/01/living-room-intro.jpg">'
-} else if (room == 'bedroom') {
-  picture = '<img src="https://st.hzcdn.com/fimgs/dcc15d8c0ab11ed0_9483-w312-h312-b0-p0--modern-bedroom.jpg">'
-} else if ( room== 'kitchen') {
-  picture = '<img src="https://www.ikea.com/images/73/8d/738dc6659e0b04b7c645012910280fe9.jpg?f=s">'
+while (room !== 'living room' && room !=='bedroom' && room !=='kitchen') {
+  room= prompt('Please enter "living room" or "kitchen" or "bedroom"');
+}
+
+total = prompt('how many rooms would you like to design?');
+for(var ctr = 0; ctr < total; ctr = ctr +1) {
+
+
+if (room == 'living room') {
+  
+  picture = picture+'<img src="https://freshome.com/wp-content/uploads/2018/01/living-room-intro.jpg">';
+}
+ else if (room == 'bedroom') {
+  picture = picture+'<img src="https://st.hzcdn.com/fimgs/dcc15d8c0ab11ed0_9483-w312-h312-b0-p0--modern-bedroom.jpg">';
+}
+ else if ( room== 'kitchen') {
+  picture = picture+'<img src="https://www.ikea.com/images/73/8d/738dc6659e0b04b7c645012910280fe9.jpg?f=s">';
 
 
 }
 
+
+}
 // output
 return picture;
+
 }
 
 
